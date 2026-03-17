c4dadff53d9646873442c757869162d2ded4c6b8
Matteo Lovato <matteolovatoai@proton.me>
2026-03-17 20:54:24 +0100

    aggiunta api per modificare una lezione, rinominato funzioni in get_lezione e insert_lezione per mantenere il singolare

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ from .database import engine, get_session
 app = FastAPI()
 
 @app.get("/lezioni/")
-def get_lezioni(session: Session = Depends(get_session), inizio: datetime | None = None, fine: datetime | None = None):
+def get_lezione(session: Session = Depends(get_session), inizio: datetime | None = None, fine: datetime | None = None):
     statement = select(Lezione, Materia.nome, Docente.cognome).select_from(Lezione).join(Modulo).join(Docente).join(Materia)
     if inizio:
         statement = statement.where(Lezione.inizio >= inizio)
@@ -29,12 +29,25 @@ def get_lezioni(session: Session = Depends(get_session), inizio: datetime | None
     return output
 
 @app.post("/lezioni/", status_code=201)
-def insert_lezioni(lezione: Lezione, session: Session = Depends(get_session)):
+def insert_lezione(lezione: Lezione, session: Session = Depends(get_session)):
     # Controllo se esiste il modulo
     modulo = session.get(Modulo, Lezione.modulo_id)
     if not modulo:
         raise HTTPException(status_code=404, detail="Modulo non trovato")
     # Aggiungo la lezione
+    session.add(lezione)
+    session.commit()
+    session.refresh(lezione)
+    return lezione
+
+@app.patch("/lezioni/{id}", response_model=Lezione)
+def update_lezione(lezione_id: int, dati_aggiornati: dict, session: Session = Depends(get_session)):
+    lezione = session.get(Lezione, lezione_id)
+    if not lezione:
+        raise HTTPException(status_code=404, detail="Lezione non trovata")
+    for chiave, valore in dati_aggiornati.items():
+        #lezione.chiave = valore
+        setattr(lezione, chiave, valore)
     session.add(lezione)
     session.commit()
     session.refresh(lezione)
