76cd46f2b186c16812d312e19d1502939858b499
Matteo Lovato <matteolovatoai@proton.me>
2026-03-31 13:33:23 +0200

    modificato end point per richiedere autenticazione

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 from sqlmodel import Session, select
-from .models import Modulo, Lezione, Docente, Classe, Modulo_classe, Modulo_docente
+from .models import Modulo, Lezione, Docente, Classe, Modulo_classe, Modulo_docente, RuoloAccesso, Utente
 from .database import get_session
 from fastapi.middleware.cors import CORSMiddleware
+from backend.auth import get_current_user
 
 app = FastAPI()
 
@@ -22,14 +23,14 @@ app.add_middleware(
 )
 
 @app.get("/lezioni/")
-def get_lezione(session: Session = Depends(get_session), inizio: datetime | None = None, fine: datetime | None = None, classe_id: int | None = None):
+def get_lezione(session: Session = Depends(get_session), inizio: datetime | None = None, fine: datetime | None = None, current_user: Utente = Depends(get_current_user)):
     statement = select(Lezione, Modulo.nome, Docente.cognome).select_from(Lezione).join(Modulo_docente).join(Docente).join(Modulo_classe).join(Modulo)
     if inizio:
         statement = statement.where(Lezione.inizio >= inizio)
     if fine:
         statement = statement.where(Lezione.fine <= fine)
-    if classe_id:
-        statement = statement.join(Modulo_classe).where(Modulo_classe.classe_id == classe_id)
+    if current_user.ruolo == RuoloAccesso.studente:
+        statement = statement.where(Modulo_classe.classe_id == current_user.ruolo)
     results = session.exec(statement).all()
 
     output = []
@@ -45,8 +46,10 @@ def get_lezione(session: Session = Depends(get_session), inizio: datetime | None
 
     return output
 
-@app.post("/lezioni/", status_code=201)
-def insert_lezione(lezione: Lezione, session: Session = Depends(get_session)):
+@app.post("/lezioni/", status_code=status.HTTP_201_CREATED)
+def insert_lezione(lezione: Lezione, session: Session = Depends(get_session), current_user: Utente = Depends(get_current_user)):
+    if current_user.ruolo != RuoloAccesso.segreteria:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Solo la segreteria può inserire le lezioni")
     # Controllo se esiste il modulo
     modulo_docente = session.get(Modulo_docente, Lezione.modulo_docente_id)
     if not modulo_docente:
@@ -64,7 +67,7 @@ def insert_lezione(lezione: Lezione, session: Session = Depends(get_session)):
         )).first()
 
     if conflitto:
-        raise HTTPException(status_code=400, detail="Conflitto, esiste già una lezione in questo slot orario per questa classe o docente")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Conflitto, esiste già una lezione in questo slot orario per questa classe o docente")
 
     # Aggiungo la lezione
     session.add(lezione)
@@ -73,7 +76,9 @@ def insert_lezione(lezione: Lezione, session: Session = Depends(get_session)):
     return lezione
 
 @app.patch("/lezioni/{id}", response_model=Lezione)
-def update_lezione(lezione_id: int, dati_aggiornati: dict, session: Session = Depends(get_session)):
+def update_lezione(lezione_id: int, dati_aggiornati: dict, session: Session = Depends(get_session), current_user: Utente = Depends(get_current_user)):
+    if current_user.ruolo != RuoloAccesso.segreteria:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Solo la segreteria può modificare le lezioni")
     lezione = session.get(Lezione, lezione_id)
     if not lezione:
         raise HTTPException(status_code=404, detail="Lezione non trovata")
@@ -86,7 +91,9 @@ def update_lezione(lezione_id: int, dati_aggiornati: dict, session: Session = De
     return lezione
 
 @app.delete("/lezioni/{id}")
-def delete_lezione(lezione_id: int, session: Session = Depends(get_session)):
+def delete_lezione(lezione_id: int, session: Session = Depends(get_session), current_user: Utente = Depends(get_current_user)):
+    if current_user.ruolo != RuoloAccesso.segreteria:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Solo la segreteria può cancellare le lezioni")
     lezione = session.get(Lezione, lezione_id)
     if not lezione:
         raise HTTPException(status_code=404, detail="Lezione non trovata")
